ea023d1754bbf5b32893b9b0afcadb195019ffd7
Gracie Miller <gracieam@ad.unc.edu>
2022-02-15 15:25:19 -0500

    Progress of ex04

--- lessons/interactice.py
+++ lessons/interactice.py
@@ -4,27 +4,30 @@ __author__ = "730402215"
 
 from turtle import Turtle, colormode, done, circle, screensize, tracer
 
+tracer(0, 0)
+drawing: Turtle = Turtle()
+
 def main() -> None:
-    tracer(0, 0)
-    sky: Turtle = Turtle()
-    grass: Turtle = Turtle()
-    star: Turtle = Turtle()
-    moon: Turtle = Turtle()
+    """The act of drawing a starry night."""
     from random import randint
-    draw_sky(sky, 0, 0)
-    draw_grass(grass, 0, 0, 300, 300)
-    draw_moon(moon, 0, 100)
-    draw_stars(star, randint(-300,300), randint(-300, 300))
+    draw_sky(drawing, 0, 0)
+    draw_grass(drawing, -400, -100 , 800, 750)
+    draw_moon(drawing, 0, 100)
+    number_of_stars(drawing)
+    draw_stars(drawing, randint(100,300), randint(100, 300))
+    
+    done()
 
 
 def draw_sky(the_sky: Turtle, x: float, y: float) -> None:
+    """Creates a solid blue background to be the night sky."""
     sky: Turtle = Turtle()
     sky.goto(x, y)
     screensize(0, 0, "blue")
 
 
 def draw_grass(landscape: Turtle, x: float, y: float, width: float, height: float) -> None:
-    """Draw a green rectangle on a third of the dark blue background."""
+    """Draw a green rectangle on a third of the blue background to be the grass."""
     grass: Turtle = Turtle()
     grass.penup()
     grass.goto(x, y)
@@ -37,13 +40,13 @@ def draw_grass(landscape: Turtle, x: float, y: float, width: float, height: floa
     while i < 2:
         grass.forward(width)
         grass.right(90)
-        grass.forward(height/2)
+        grass.forward(height/3)
         grass.right(90)
         i += 1
     grass.end_fill()
 
 def draw_stars(star: Turtle, x: float, y: float) -> None:
-    """Draw yellow stars on dark blue background."""
+    """Draw yellow stars on a blue background."""
     stars: Turtle = Turtle()
     stars.pencolor("yellow")
     stars.fillcolor("yellow")
@@ -53,16 +56,28 @@ def draw_stars(star: Turtle, x: float, y: float) -> None:
     
     stars.begin_fill()
     i: int = 0 
+    subtract_angle: int = 72
     while i < 5:
         stars.forward(15)
         stars.right(120)
         stars.forward(15)
-        stars.right(72 - 120)
+        stars.right(subtract_angle - 120)
         i += 1
     stars.end_fill()
 
+
+def number_of_stars(star: Turtle) -> None:
+    """Creating a random number of stars."""
+    from random import randint
+    star_N: int = randint(10, 20)
+    i: int = 0
+    while i < star_N:
+        draw_stars(drawing, randint(-350,350), randint(100, 300))
+        i += 1
+
+
 def draw_moon(the_moon: Turtle, x: float, y: float) -> None:
-    """Draw a moon on dark blue background."""
+    """Draw a moon on the blue background."""
     moon: Turtle = Turtle()
     moon.hideturtle()
     moon.penup()
@@ -79,4 +94,3 @@ def draw_moon(the_moon: Turtle, x: float, y: float) -> None:
 if __name__ == "__main__":
     main()
 
-done()
