023d0da9c7e67f75da26c3513ffc4484edf4bca0
Gracie Miller <gracieam@ad.unc.edu>
2022-05-23 21:36:01 -0400

    Finished course

--- lessons/interactice.py
+++ lessons/interactice.py
@@ -1,12 +1,11 @@
+def grocery_list(inventory: dict[str, int], g_list: dict[str, int]) -> list[str]:
+    result: list[str] = []
+    for item in g_list:
+        if item in inventory:
+            if g_list[item] <= inventory[item]:
+                result.append(item)
+    return result
 
-
-
-def indices_word(word: str, char: str) -> str:
-    """"""
-    index: str = ""
-    i: int = 0
-    while i < len(word):
-        if char == word[i]:
-            index += word[i]
-    i += 1
-    return index
+inventory: dict[str, int] = {"bread" : 3, "chips" : 1, "eggs" : 5}
+g_list: dict[str, int] = {"bread" : 2, "chips" : 10, "eggs" : 1}
+print(grocery_list(inventory, g_list))
